b44e10c16e5204f743b477aa5265f55780f7ef8f
kohta.k <kohta0421kochi@gmail.com>
2022-07-14 16:57:48 +0900

    変数宣言

--- lesson.go
+++ lesson.go
@@ -1,12 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"os/user"
-	"time"
+import "fmt"
+
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false
 )
 
+func foo() {
+	xi := 1
+	xf64 := 1.6
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xf64)
+	fmt.Printf("%T\n", xi)
+}
+
 func main() {
-	fmt.Println("Hello, world!", time.Now())
-	fmt.Println(user.Current())
+	fmt.Println(i, f64, s, t, f)
+	foo()
 }
