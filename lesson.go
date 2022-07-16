2243d48d64ed9beeffef40080c8c3933895cf5c7
kohta.k <kohta0421kochi@gmail.com>
2022-07-16 15:46:19 +0900

    7/16 start

--- lesson.go
+++ lesson.go
@@ -2,13 +2,7 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
-
-const (
-	Username = "test_user"
-	Password = "test_pass"
-)
-
 func main() {
-	fmt.Println(Pi, Username, Password)
+	t, f := true, false
+	fmt.Printf("%T, %v\n, t, t")
 }
