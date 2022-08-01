51f881b822c1d6f5f63029dd8d647c3907fefdf7
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-01 21:48:45 +0900

    変数宣言　復習

--- lesson.go
+++ lesson.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var (
-		i    int     = 1
-		f64  float64 = 1.2
-		s    string  = "test"
-		t, f bool    = true, false
-	)
-	fmt.Println(i, f64, s, t, f)
 
-	xi := 1
-	xf64 := 1.2
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
 }
