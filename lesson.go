fd286ea33a71fb18a1129dc19299835506120771
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-01 21:53:39 +0900

    const

--- lesson.go
+++ lesson.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+func main() {
+	fmt.Println(Pi, Username, Password)
 }
