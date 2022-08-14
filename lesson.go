fa017541e94e71110b0fce9c7c1581955dadb00e
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-14 14:55:25 +0900

    go 2022/08/14

--- lesson.go
+++ lesson.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
-
-const Pi = 3.14
-
-const (
-	Username = "test_user"
-	Password = "test_pass"
+import (
+	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(Pi, Username, Password)
+	fmt.Println("hello world!")
+	fmt.Println("hello" + "world")
+	fmt.Println(string("Hello World"[0]))
+
+	var s string = "Hello World"
+	fmt.Println(strings.Replace(s, "H", "H", 1))
+
+	fmt.Println(strings.Contains(s, "World"))
+
+	fmt.Println("Test\n" +
+		"Test")
 }
