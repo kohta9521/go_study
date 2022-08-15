e21e820ecb897cecfce6f9adf69a4aafbd909987
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-15 17:52:55 +0900

    2022/08/15 first commit

--- section3/13.go
+++ section3/13.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var x int = 1
 	xx := float64(x)
 	fmt.Printf("%T %v %f\n", xx, xx, xx)
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
+
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	fomt.Printf("%T %v %d\n", i, i)
 }
