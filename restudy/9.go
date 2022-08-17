70fb20c8533f6de402949bea8b55cd65fe4f7af3
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-17 13:02:46 +0900

    pーバーフロー

---
+++ restudy/9.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+var big int = 4390579752
+
+func main() {
+	fmt.Println(Pi, Username, Password)
+	fmt.Println(big)
+}
