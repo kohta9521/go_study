a76d29cad05e0363bca9b54aff79acb04d20c5af
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-11 15:04:26 +0900

    string

--- youtube/section7/sample.go
+++ youtube/section7/sample.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
+	var string_a string = "Hello,World!!"
+	string_b := "Hello,World"
 	num01 := 123
 	var num02 int = 1234567890
 	num03 := 1.23
 	var num04 float64 = 1.23456789
+
+	fmt.Println(string_a)
+	fmt.Println(reflect.TypeOf(string_a))
+
+	fmt.Println(string_b)
+	fmt.Println(reflect.TypeOf(string_b))
 
 	fmt.Println(reflect.TypeOf(num01))
 	fmt.Println(reflect.TypeOf(num02))
