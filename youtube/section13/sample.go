1252b284f5037a63633d6770a7d09940b98b5971
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-13 23:01:25 +0900

    構造体 zissou

--- youtube/section13/sample.go
+++ youtube/section13/sample.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+type Student struct {
+	name          string
+	math, english float64
+}
+
+func main() {
+	var s Student
+	s.name = "sato"
+	s.math = 80
+	s.english = 70
+
+	fmt.Println(s)
 }
