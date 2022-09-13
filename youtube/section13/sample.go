85a384ff12c5391b72eec46d347b4d6f239a303e
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-13 23:51:08 +0900

    オライリー書籍での勉強

--- youtube/section13/sample.go
+++ youtube/section13/sample.go
@@ -9,9 +9,11 @@ type Student struct {
 
 func main() {
 	var s Student
-	s.name = "sato"
-	s.math = 80
-	s.english = 70
+	// s.name = "sato"
+	// s.math = 80
+	// s.english = 70
+
+	s := Student{"sato", 80, 70}
 
 	fmt.Println(s)
 }
