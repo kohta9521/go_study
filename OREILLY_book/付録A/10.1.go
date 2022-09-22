f4e7dc9bf86b7d93c3a47f0ae7a0f04634705c5f
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-22 15:26:25 +0900

    変数宣言とセットのifの利用方法が理解薄い

--- OREILLY_book/付録A/10.1.go
+++ OREILLY_book/付録A/10.1.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var statusCode int = 200
+
+	if statusCode == 200 {
+		fmt.Println("no error")
+	} else if statusCode < 500 {
+		fmt.Println("client erro")
+	} else {
+		fmt.Println("erverError")
+	}
+
+	if result, ok := cache[input]; ok {
+		for i, s := range scketches {
+			fmt.Println(i, s)
+		}
+	}
+
+}
