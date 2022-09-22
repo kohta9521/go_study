6ab966d8cdbffb08d623a88c8e970ce80ea8acf8
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-22 14:51:27 +0900

    map関数　ようわからん

--- Todoapp/main.go
+++ Todoapp/main.go
@@ -1,16 +1,15 @@
-
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/", hoge)
-    r.Run() // デフォルトが8080ポートなので今回は変更しません
+	r := gin.Default()
+	r.GET("/", hoge)
+	r.Run() // デフォルトが8080ポートなので今回は変更しません
 }
 
 func hoge(c *gin.Context) {
-    c.JSON(200, "hogeeeeeeee")
+	c.JSON(200, "hogeeeeeeee")
 }
