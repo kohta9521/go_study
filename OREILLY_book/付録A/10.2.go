6b725ee3d0219da1c83de65bb4addbc9dc61af04
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-22 15:40:10 +0900

    for文　伝統的な書き方と周り続けるループ

--- OREILLY_book/付録A/10.2.go
+++ OREILLY_book/付録A/10.2.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
 	scketches := []string{"Dead Porrot", "Killer joke", "Spanish Inquisition", "Spam"}
@@ -16,5 +20,35 @@ func main() {
 	//ブランク識別子でインデックスを読みとバイsて値だけを扱う
 	for _, s := range scketches {
 		fmt.Println(s)
+	}
+
+	//breakなどで読み飛ばす処理
+	for _, s := range scketches {
+		if strings.HasPrefix(s, "K") {
+			continue
+		}
+		if strings.HasSuffix(s, "n") {
+			break
+		}
+	}
+
+	//ブール型の要素を１つだけ持つループ文
+	counter := 0
+	for counter < 10 {
+		fmt.Println("ブール値がtrueの間回り続けるループ")
+		counter += 1
+	}
+
+	end := time.Now().Add(time.Second)
+	for {
+		fmt.Println("brealやreturnで抜けないと終わらないループ")
+		if end.Before(time.Now()) {
+			break
+		}
+	}
+
+	//伝統的なループ処理
+	for i := 0; i < 10; i++ {
+		fmt.Println("10回繰り替えす")
 	}
 }
