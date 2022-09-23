325d1ab6b0b47798e9cdfb194a18be15e86632a2
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-23 14:53:03 +0900

    interface make

--- basic/section4/4_20.go
+++ basic/section4/4_20.go
@@ -9,4 +9,14 @@ import (
 func main() {
 	var x interface{}
 	fmt.Println(x)
+
+	x = 1
+	fmt.Println(x)
+	x = "A"
+	fmt.Println(x)
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
+
+	x = 2
+	fmt.Println(x + 3)
 }
