6015114781da654210c7ae355e62e5f6a464349f
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-03 17:02:39 +0900

    8_39 型アサーション　難しい　要復習

--- basic/section8/8_39.go
+++ basic/section8/8_39.go
@@ -1,31 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //switch
 //型スイッチ
 
 func anything(a interface{}) {
-	fmt.Println(a)
+	// fmt.Println(a)
+	switch v := a.(type) {
+	case string:
+		fmt.Println(v + "!?")
+	case int:
+		fmt.Println(v + 10000)
+	}
 }
 
 func main() {
+	//型アサーション　動的に型を判定
 	anything("aaa")
 	anything(1)
-
-	//型アサーション
 
 	var x interface{} = 3
 	i := x.(int)
 	fmt.Println(i + 2)
-	//fmt.Println(x + 2)
+	// fmt.Println(x + 2)
 
-	f := x.(float64)
-	fmt.Println(f)
+	// f := x.(float64)
+	// fmt.Println(f)
 
-	//停止しない方法
+	//型アサーションでも絵エラーを起こさない
 	f, isFloat64 := x.(float64)
 	fmt.Println(f, isFloat64)
 
@@ -34,11 +37,12 @@ func main() {
 	} else if i, isInt := x.(int); isInt {
 		fmt.Println(i, "x is Int")
 	} else if s, isString := x.(string); isString {
-		fmt.Println(s, "isString")
+		fmt.Println(s, isString)
 	} else {
 		fmt.Println("I don't know")
 	}
 
+	//型によるスイッチ文
 	switch x.(type) {
 	case int:
 		fmt.Println("int")
@@ -46,5 +50,14 @@ func main() {
 		fmt.Println("string")
 	default:
 		fmt.Println("I don't know")
+	}
+
+	switch v := x.(type) {
+	case bool:
+		fmt.Println(v, "bool")
+	case int:
+		fmt.Println(v, "int")
+	case string:
+		fmt.Println(v, "string")
 	}
 }
