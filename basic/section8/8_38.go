ec8ae5195928e8b6dc685d652cd2a790aeb3f778
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-03 16:26:17 +0900

    8_38 restudy

--- basic/section8/8_38.go
+++ basic/section8/8_38.go
@@ -8,30 +8,13 @@ import (
 //式スイッチ
 
 func main() {
-	// n := 5
-	// switch n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I don't know")
-	// }
-
-	// switch n := 2; n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I don't know")
-	// }
-
-	n := 6
-	switch {
-	case n > 0 && n < 4:
-		fmt.Println("0 < n < 4")
-	case n > 3 && n < 7:
-		fmt.Println("3 < n < 7")
+	n := 1
+	switch n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("I don't know")
 	}
 }
