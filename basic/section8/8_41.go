c724dee129f53cbf0ed388546185f490c4fd6685
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-03 17:24:49 +0900

    deferを使用したリソース解放処理

---
+++ basic/section8/8_41.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//defer
+func TestDefer() {
+	defer fmt.Println("END")
+	fmt.Println("START")
+}
+
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+}
+func main() {
+	TestDefer()
+
+	// defer func() {
+	// 	fmt.Println("1")
+	// 	fmt.Println("2")
+	// 	fmt.Println("3")
+	// }()
+
+	RunDefer()
+
+	//リソースの解放処理
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	file.Write([]byte("Hello"))
+
+}
