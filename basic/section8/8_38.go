65c774ae83df9fabc666913b04876e28ad68a031
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-03 16:34:43 +0900

    switch部分で文字列と複合演算子を同時に行うとエラーが発生する

--- basic/section8/8_38.go
+++ basic/section8/8_38.go
@@ -17,4 +17,32 @@ func main() {
 	default:
 		fmt.Println("I don't know")
 	}
+
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("I don't know")
+	}
+
+	a := 6
+	switch {
+	case a > 0 && a < 4:
+		fmt.Println("0 < a < 4")
+	case a > 3 && a < 7:
+		fmt.Println("3 < a < 7")
+	}
+
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	case n < 3 && n < 6:
+		fmt.Println("3 > n < 6")
+	default:
+		fmt.Println("I don't know")
+	}
 }
