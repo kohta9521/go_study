bbc1ef7d784f996e4013f10e2d81c54025eefdd5
kohta  <kohtakochi@192.168.0.22>
2022-10-04 19:53:03 +0900

    constans

--- basic/section6/6_26.go
+++ basic/section6/6_26.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+//論理演算子
+
+func main() {
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+
+	fmt.Println(true || false == true)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
 }
