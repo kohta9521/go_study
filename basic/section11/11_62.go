05374b6da7a97c08d6e57d439bc00d324c29060b
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-08 17:51:21 +0900

    値渡し　参照渡し　気をつけるべきポイント

---
+++ basic/section11/11_62.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	Name string
+	Age  int
+	// X, Y int
+}
+
+func main() {
+	var user1 User
+	fmt.Println(user1)
+	user1.Name = "user1"
+	user1.Age = 10
+	fmt.Println(user1)
+
+	user2 := User{}
+	fmt.Println(user2)
+	user2.Name = "user2"
+	fmt.Println(user2)
+
+	user3 := User{Name: "user3", Age: 30}
+	fmt.Println(user3)
+}
