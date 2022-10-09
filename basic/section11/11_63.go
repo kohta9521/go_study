97d59bee85ab4bd6a1fb12309a06fee30a510ccf
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-09 09:33:02 +0900

    ポインタ型とか出てきてようわからん

--- basic/section11/11_63.go
+++ basic/section11/11_63.go
@@ -13,7 +13,21 @@ func (u User) SayName() {
 	fmt.Println(u.Name)
 }
 
+func (u User) SetName(name string) {
+	u.Name = name
+}
+
+func (u *User) SetName2(name string) {
+	u.Name = name
+}
+
 func main() {
 	user1 := User{Name: "user1"}
+	user1.SayName()
+
+	user1.SetName("A")
+	user1.SayName()
+
+	user1.SetName2("A")
 	user1.SayName()
 }
