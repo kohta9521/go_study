6b178134f4ff915a5e3fd6b517ceb41f38ddf254
kohta  <kohtakochi@192.168.0.33>
2022-10-11 18:15:45 +0900

    varではなくconstだとoverflowしない

--- restudy/section3/8.go
+++ restudy/section3/8.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 //変数宣言
+//まずは代入の方法
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false
+)
 
 func main() {
 	//まずは代入の方法
@@ -14,4 +21,18 @@ func main() {
 	)
 
 	fmt.Println(i, f64, s, t, f)
+
+	foo()
+}
+
+//適当な関数を設定してみた
+func foo() {
+	//関数内部でのみ設定可能
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+
+	fmt.Printf("%T", xf64)
 }
