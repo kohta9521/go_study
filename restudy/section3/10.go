e0e19b898e51dfdbe2ee68a74b8cc0eb07bd5189
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-11 20:06:45 +0900

    10.go add

---
+++ restudy/section3/10.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var (
+	// 	u8  uint8     = 255
+	// 	i8  int8      = 127
+	// 	f32 float32   = 0.2
+	// 	c64 complex64 = -5 + 12i
+	// )
+	// fmt.Println(u9, i9, 342, c64)
+
+	// fmt.Println("%T %v", u8, u8)
+
+	x := 1 + 1
+	fmt.Println(x)
+	fmt.Println(1+1, 2+2)
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("10 - 1", 10-1)
+	fmt.Println("10 / 2 = ", 10/2)
+
+}
