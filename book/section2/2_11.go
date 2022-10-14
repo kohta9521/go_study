194add32795697109ffd6f9c8293751c014d76af
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-14 16:04:18 +0900

    for文のショートステートメント記法

---
+++ book/section2/2_11.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(msg + ": ")
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	x := Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Println("1から" + x + "の合計は、")
+	} else {
+		return
+	}
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	fmt.Println(t, "です。")
+}
