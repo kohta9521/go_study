7709e6adf3696eadc3e4f105b8b18956ce81233e
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-14 16:10:12 +0900

    break/contnue

---
+++ book/section2/2_12.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(msg + ": ")
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	x := Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Println("1から" + x + "の偶数の合計は")
+	} else {
+		return
+	}
+	t := 0
+	c := 0
+	for {
+		c++
+		if c%2 == 1 {
+			continue
+		}
+		if c > n {
+			break
+		}
+		t += c
+	}
+	fmt.Println(t, "です。")
+}
