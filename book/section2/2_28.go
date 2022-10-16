fe7f63af7030bfad99491abda38656f74f667f7e
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-16 15:32:53 +0900

    2_28 error

---
+++ book/section2/2_28.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	modify := func(a []string, f func([]string) [string] []string {
+		return f(a)
+	}
+
+	m := []string {
+		"1st", "2nd", "3rd",
+	}
+	fmt.Println(m)
+
+	m1 := modify(m, func([]string) []string {
+		return append(m, m...)
+	})
+	fmt.Println(m1)
+	
+	m2 := modify(m, func([]string) []string {
+		return m[:len(m)-1]
+	})
+	fmt.Println(m2)
+
+	m3 := modify(m, func([]string) []string {
+		return m[1:]
+	})
+	fmt.Println(m3)
+}
