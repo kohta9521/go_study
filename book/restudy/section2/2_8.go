706f6902938d5cc9c461ac3692492b94caeaa887
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-19 00:20:17 +0900

    fallthough

---
+++ book/restudy/section2/2_8.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(msg + ": ")
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	x := Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print(x, "は、")
+	} else {
+		return
+	}
+	switch {
+	case n%2 == 0:
+		fmt.Println("偶数です")
+	case n%2 == 1:
+		fmt.Println("奇数です")
+	}
+}
