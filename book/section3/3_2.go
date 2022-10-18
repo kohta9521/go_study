ecda7559c50d6cdc749ec1fee011aa4f67c13741
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-18 15:45:13 +0900

    point change

--- book/section3/3_2.go
+++ book/section3/3_2.go
@@ -11,4 +11,9 @@ func main() {
 	p2 := &m
 	fmt.Printf("p value:%d, address:%p\n", *p, p)
 	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
 }
