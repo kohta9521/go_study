e3f4bb9667f55f86da34e3c3875562cbb6df26b8
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-18 16:19:03 +0900

    struct add

---
+++ book/section3/3_5.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ar := []int{10, 20, 30}
+	fmt.Println(ar)
+	initial(&ar)
+	fmt.Println(ar)
+}
+
+func initial(ar *[]int) {
+	for i := 0; i < len(*ar); i++ {
+		(*ar)[i] = 0
+	}
+}
---
+++ book/section3/3_6.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var mydata struct {
+	Name string
+	Data []int
+}
+
+func main() {
+	mydata.Name = "Taro"
+	mydata.Data = []int{10, 20, 30}
+	fmt.Println(mydata)
+}
