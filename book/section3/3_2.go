cc423373fc7a550b8e649ba344e52446fdbd32da
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-18 15:43:06 +0900

    adress add comp

---
+++ book/section3/3_2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 123
+	p := &n
+	m := 10000
+	p2 := &m
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+}
