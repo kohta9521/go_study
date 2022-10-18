6bc2e28ab4ec6cab3ed18628534fae74a4f28d33
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-18 16:22:53 +0900

    2 paterun strunct

---
+++ book/section3/3_7.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Mydata struct {
+	Name string
+	Data []int
+}
+
+func main() {
+	taro := Mydata{"Taro", []int{10, 20, 30}}
+	hanako := Mydata{
+		Name: "Hanako",
+		Data: []int{90, 80, 70},
+	}
+	fmt.Println(taro)
+	fmt.Println(hanako)
+}
