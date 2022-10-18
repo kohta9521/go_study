4ef9868c6db624ed1050bf8600bd216bea5683ea
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-18 15:55:41 +0900

    pointa only change

---
+++ book/section3/3_3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 123
+	p := &n
+	q := &p
+	m := 10000
+	p2 := &m
+	q2 := &p2
+	fmt.Printf("q value:%d, adress:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, adress:%p\n", **q2, *q2)
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("q value:%d, adress:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, adress:%p\n", **q2, *q2)
+}
---
+++ book/section3/3_4.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 123
+	fmt.Printf("value:%d.\n", n)
+	change1(n)
+	fmt.Printf("value:%d.\n", n)
+	change2(&n)
+	fmt.Printf("value:%d.\n", n)
+}
+
+func change1(n int) {
+	n *= 2
+}
+
+func change2(n *int) {
+	*n *= 2
+}
