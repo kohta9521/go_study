4d51d4a64becb0534c7244b0f96c8e9d72165ddf
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-20 19:43:42 +0900

    2022/10/20 first commit

---
+++ book/restudy/section2/2_15.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(msg + ": ")
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	x := Input("input data")
+	ar := strings.Split(x, " ")
+	t := 0
+	for _, v := range ar {
+		n, er := strconv.Atoi(v)
+		if er != nil {
+			goto err
+		}
+		t += n
+	}
+	fmt.Println("total:", t)
+	return
+
+err:
+	fmt.Println("ERROR!!")
+}
